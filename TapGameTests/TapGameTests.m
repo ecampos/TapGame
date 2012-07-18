89ae831ca8b643d242be43f8f333997925fbf3bb
ecampos <campos.e@gmail.com>
2012-07-18 17:11:17 +0200

    Initial Commit

---
+++ TapGameTests/TapGameTests.m
@@ -0,0 +1,32 @@
+//
+//  TapGameTests.m
+//  TapGameTests
+//
+//  Created by Emanuel Campos on 18.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TapGameTests.h"
+
+@implementation TapGameTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TapGameTests");
+}
+
+@end
